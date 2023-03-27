fc1b5da81144c6ea743909052a7e2dc550911661
Wayne Graham <wgraham@clir.org>
2023-03-27 12:23:22 -0400

    Add Search (and additional reports)
    Adds static search index generator
    Adds (simple) search ability
    Adds additional reports

---
+++ assets/js/search.js
@@ -0,0 +1,47 @@
+// using proxy is facter than Object.fromEntries()
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+let query = params.q;
+
+function displaySearchResults(results) {
+    let searchResults = document.getElementById('search-results');
+
+    // @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString
+    const options = {
+        year: "numeric",
+        month: "long"
+    };
+
+    if(results.length) {
+
+        let appendString='';
+
+        for(var i = 0; i < results.length; i++){
+            let item = store[results[i].ref];
+            // TODO: mustach template
+            let date = new Date(item.pub_date).toLocaleString('en-US', options);
+
+            appendString += '<p><a href="' + item.url + '">' + item.title + '</a> by ' + item.authors + '. ' + date + '. ' + item.clir_id + '.</p>';
+        }
+        searchResults.innerHTML = appendString;
+
+
+    } else {
+        searchResults.innerHTML = '<li>No results found</li>';
+    }
+}
+
+console.log('store', store);
+
+if(query) {
+    // set search box value
+    document.getElementById('search-box').setAttribute('value', query);
+
+    var idx = lunr.Index.load(search_index); // search pre-build search index
+    let results = idx.search(query); // return lunr search object
+
+    displaySearchResults(results);
+}
---
+++ build-index.js
@@ -0,0 +1,75 @@
+const lunr = require('lunr');
+const glob = require('glob');
+const fs = require('fs');
+const path = require('path');
+const yaml = require("js-yaml");
+const S = require('string');
+
+const documents = {};
+const baseUrl = yaml.load(fs.readFileSync(path.join(__dirname, '/_config.yml'))).baseurl;
+
+const files = glob.sync(path.join(__dirname, '/_reports/**/*.md'));
+files.forEach((file) => {
+    const fileString = fs.readFileSync(file).toString();
+    const content = fileString.split('---');
+    const frontMatter = yaml.load(content[1]);
+    // clir_id: pub51
+    // layout: publication
+    // title: "Preservation Activities in Bulgaria: The State of Affairs and Possibilities for Cooperation"
+    // description: Traces the literary history of Bulgaria and the state of its preservation activities. Concludes with general observations and a list of future directions compiled by Bulgarian libraries.
+    // isbn: 1-887334-36-X
+    // eric: ED379003
+    // doi: 10.5281/zenodo.7750633
+    // subjects: Background, Centralization, Decentralization, Economics, Foreign Countries, History, Library Collections, Library Development, Library Education, Library Networks, Library Statistics, Political Issues, Preservation, Problems, Research Libraries
+    // authors: Jordan, Sonja K.
+    // pub_date: "1995-02-01"
+    // pages: 12
+    const fileData = {
+        clir_id: frontMatter.clir_id,
+        title: frontMatter.title,
+        authors: frontMatter.authors,
+        description: S(frontMatter.description).stripPunctuation(),
+        isbn: frontMatter.isbn,
+        doi: frontMatter.doi,
+        pub_date: frontMatter.pub_date,
+        pages: frontMatter.pages,
+        content: S(content[2]).escapeHTML().stripTags().stripPunctuation().s,
+        url: `${baseUrl}/reports/${file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))}/`
+    };
+    documents[file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))] = fileData;
+});
+
+const builder = new lunr.Builder();
+builder.ref('id');
+builder.field('title', { boost: 10 });
+builder.field('author');
+builder.field('category', { boost: 5 });
+builder.field('content', { boost: 15 });
+builder.metadataWhitelist = ['position'];
+
+builder.pipeline.add(
+    lunr.trimmer,
+    lunr.stopWordFilter,
+    lunr.stemmer
+);
+
+builder.searchPipeline.add(
+    lunr.trimmer,
+    lunr.stopWordFilter,
+    lunr.stemmer
+);
+
+for (var key in documents) { // Add the data to lunr
+    builder.add({
+        'id': key,
+        'title': documents[key].title,
+        'author': documents[key].author,
+        'category': documents[key].category,
+        'content': documents[key].content
+    });
+}
+const index = builder.build();
+const index_data = `var search_index = ${JSON.stringify(index, null, 4)};`;
+const documents_data = `var store = ${JSON.stringify(documents, null, 4)};`;
+fs.writeFileSync(path.join(__dirname,'/assets/js/document-store.js'), documents_data);
+fs.writeFileSync(path.join(__dirname,'/assets/js/search-index.js'), index_data);
