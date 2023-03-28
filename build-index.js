a0a98bf96a5beba701fab898bfb1f6ee9dbf48ed
Wayne Graham <wgraham@clir.org>
2023-03-28 13:22:08 -0400

    compile and minify

--- build-index.js
+++ build-index.js
@@ -1,17 +1,19 @@
-const lunr = require('lunr');
-const glob = require('glob');
-const fs = require('fs');
-const path = require('path');
+const lunr = require("lunr");
+const glob = require("glob");
+const fs = require("fs");
+const path = require("path");
 const yaml = require("js-yaml");
-const S = require('string');
+const S = require("string");
 
 const documents = {};
-const baseUrl = yaml.load(fs.readFileSync(path.join(__dirname, '/_config.yml'))).baseurl;
+const baseUrl = yaml.load(
+    fs.readFileSync(path.join(__dirname, "/_config.yml"))
+).baseurl;
 
-const files = glob.sync(path.join(__dirname, '/_reports/**/*.md'));
+const files = glob.sync(path.join(__dirname, "/_reports/**/*.md"));
 files.forEach((file) => {
     const fileString = fs.readFileSync(file).toString();
-    const content = fileString.split('---');
+    const content = fileString.split("---");
     const frontMatter = yaml.load(content[1]);
     // clir_id: pub51
     // layout: publication
@@ -34,44 +36,89 @@ files.forEach((file) => {
         pub_date: frontMatter.pub_date,
         pages: frontMatter.pages,
         content: S(content[2]).escapeHTML().stripTags().stripPunctuation().s,
-        url: `${baseUrl}/reports/${file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))}/`
+        url: `${baseUrl}/reports/${file.substring(
+            file.lastIndexOf("/") + 1,
+            file.lastIndexOf(".")
+        )}/`,
     };
-    documents[file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.'))] = fileData;
+    documents[file.substring(file.lastIndexOf("/") + 1, file.lastIndexOf("."))] =
+        fileData;
 });
 
 const builder = new lunr.Builder();
-builder.ref('id');
-builder.field('title', { boost: 10 });
-builder.field('authors');
-builder.field('description', { boost: 15 });
-builder.field('content');
-builder.field('pub_date');
-builder.metadataWhitelist = ['position'];
+builder.ref("id");
+builder.field("title", { boost: 10 });
+builder.field("authors");
+builder.field("description", { boost: 15 });
+builder.field("content");
+builder.field("pub_date");
+builder.metadataWhitelist = ["position"];
 
-builder.pipeline.add(
-    lunr.trimmer,
-    lunr.stopWordFilter,
-    lunr.stemmer
-);
+builder.pipeline.add(lunr.trimmer, lunr.stopWordFilter, lunr.stemmer);
 
-builder.searchPipeline.add(
-    lunr.trimmer,
-    lunr.stopWordFilter,
-    lunr.stemmer
-);
+builder.searchPipeline.add(lunr.trimmer, lunr.stopWordFilter, lunr.stemmer);
 
-for (var key in documents) { // Add the data to lunr
+for (var key in documents) {
+  // Add the data to lunr
     builder.add({
-        'id': key,
-        'title': documents[key].title,
-        'authors': documents[key].authors,
-        'pub_date': documents[key].pub_date,
-        'description': documents[key].description,
-        'content': documents[key].content
+        id: key,
+        title: documents[key].title,
+        authors: documents[key].authors,
+        pub_date: documents[key].pub_date,
+        description: documents[key].description,
+        content: documents[key].content,
     });
 }
 const index = builder.build();
 const index_data = `var search_index = ${JSON.stringify(index, null, 4)};`;
 const documents_data = `var store = ${JSON.stringify(documents, null, 4)};`;
-fs.writeFileSync(path.join(__dirname,'/assets/js/document-store.js'), documents_data);
-fs.writeFileSync(path.join(__dirname,'/assets/js/search-index.js'), index_data);
+fs.writeFileSync(
+    path.join(__dirname, "/assets/js/document-store.js"),
+    documents_data
+);
+fs.writeFileSync(
+        path.join(__dirname, "/assets/js/search-index.js"),
+        index_data
+    );
+
+    // @see https://www.digitalocean.com/community/tools/minify
+
+    const { minify } = require("terser");
+    // Define the config for how Terser should minify the code
+    // This is set to how you currently have this web tool configured
+    const config = {
+    compress: {
+        dead_code: true,
+        drop_console: false,
+        drop_debugger: true,
+        keep_classnames: false,
+        keep_fargs: true,
+        keep_fnames: false,
+        keep_infinity: false,
+    },
+    mangle: {
+        eval: false,
+        keep_classnames: false,
+        keep_fnames: false,
+        toplevel: false,
+        safari10: false,
+    },
+    module: false,
+    sourceMap: false,
+    output: {
+        comments: "some",
+    },
+};
+
+const searchIndex = fs.readFileSync("assets/js/search-index.js", "utf8");
+const documentStore = fs.readFileSync("assets/js/document-store.js", "utf8");
+
+// const minified_index = minify(searchIndex, config);
+// const minified_docs = minify(documentStore, config);
+
+// console.log(minified_docs.code);
+
+// fs.writeFileSync(path.join(__dirname,'/assets/js/document-store.min.js'), minified_docs);
+// fs.writeFileSync(path.join(__dirname,'/assets/js/search-index.min.js'), minified_index);
+// fs.writeFileSync('assets/js/search-index.min.js', minified.code);
+// fs.writeFileSync('assets/js/document-store.min.js', minified_docs.code);
